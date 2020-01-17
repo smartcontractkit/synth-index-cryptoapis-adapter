2b227cb6d9f2b8fd162a433868ca291b35c3011c
Jonas Hals <contact@jonashals.me>
2020-01-17 20:33:01 +0100

    Improve error handling

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const getPriceData = async (offset) => {
 const calculateIndex = (indexes) => {
     let value = new Decimal(0);
     indexes.forEach(i => {
-        value = value.plus(new Decimal(i.units).times(new Decimal(i.priceData.price)));
+        const price = i.priceData.price;
+        if (price <= 0)
+            throw "invalid price";
+        value = value.plus(new Decimal(i.units).times(new Decimal(price)))
     });
     return value.toNumber()
 };
@@ -35,9 +38,26 @@ const createRequest = async (input, callback) => {
 
     await Promise.all(data.index.map(async (synth) => {
         synth.priceData = results.find(d => typeof d.originalSymbol !== "undefined" && d.originalSymbol.toLowerCase() === synth.symbol.toLowerCase())
-    }));
+    })).catch(err => {
+        callback(500, {
+            jobRunID: input.id,
+            status: 'errored',
+            error: err,
+            statusCode: 500
+        })
+    });
 
-    data.result = calculateIndex(data.index);
+    try {
+        data.result = calculateIndex(data.index);
+    } catch (e) {
+        callback(500, {
+            jobRunID: input.id,
+            status: 'errored',
+            error: "failed getting price",
+            statusCode: 500
+        });
+        return
+    }
 
     callback(200, {
         jobRunID: input.id,
